55dbf82f6c26333ecb7d0a088ec8480e4718d390
Lázaro Clapp <lazaro@stanford.edu>
2012-11-07 05:01:09 -0800

    Added .gitignore.

--- htdocs/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-    // Database connection
-    $link = mysql_connect('mysql-user-master.stanford.edu', 'ccs147huiminl', 'teicheev');
-    mysql_set_charset('utf8');
-    mysql_select_db('c_cs147_huiminl');
-?>
